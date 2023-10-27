b5b95dffa3c354ba6041e8b0a56c6509cc2a08ac
0x6a69616e <0x6a69616e@gmail.com>
2023-10-27 13:51:56 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const
+  axios = require('axios'),
+  { Transform } = require('stream');
+
+module.exports = class {
+  static async new_chat(user_id) {
+    return await axios({
+      url: 'https://jarvischat.app/new_chat',
+      method: 'POST',
+      data: JSON.stringify({
+        user_id
+      })
+    })
+  }
+  
+  static async chat_api_stream(question, chat_id) {
+    const {
+      data
+    } = await axios({
+      url: 'https://jarvischat.app/chat_api_stream',
+      method: 'POST',
+      responseType: 'stream',
+      data: JSON.stringify({
+        question,
+        chat_id,
+        timestamp: Date.now()
+      })
+    });
+
+    function modifyChunk(chunk) {
+      const
+        line = new TextDecoder().decode(chunk).trimRight(),
+        index = line.indexOf(':');
+
+      if (index <= 0) return;
+
+      const field = line.substring(0, index);
+      if (field !== 'data') return;
+
+      const value = line.substring(index + 1).trimLeft();
+      return value;
+    }
+
+    return data.pipe(new Transform({
+      transform(chunk, encoding, callback) {
+        let modifiedChunk = modifyChunk(chunk);
+        callback(null, modifiedChunk);
+      }
+    }))
+  }
+}
